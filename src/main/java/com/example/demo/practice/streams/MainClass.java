3809076ec813592785d3a1d8db4bd8ff58317721
roshinivr <roshinirajesh1@gmail.com>
2023-01-05 22:07:02 +0000

    groupPeopleByNameAndReturnAListOfAges using lambda

--- src/main/java/com/example/demo/practice/streams/MainClass.java
+++ src/main/java/com/example/demo/practice/streams/MainClass.java
@@ -7,15 +7,35 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toList;
 
 public class MainClass {
     public static void main(String[] args) {
 
         System.out.println(getOddAge());
+        //[Person[name=Cathy, age=23], Person[name=Chinnu, age=1], Person[name=Batman, age=9], Person[name=Nancy, age=25]]
+
         System.out.println(getEvenAge());
+        //[Person[name=Sara, age=20], Person[name=Nancy, age=22], Person[name=Kitty, age=4], Person[name=Cinderella, age=10]]
+
         System.out.println(getByUsingPartitioningByForEvenAndOddAges());
+        /*
+         * {false=[Person[name=Cathy, age=23], Person[name=Chinnu, age=1], Person[name=Batman, age=9], Person[name=Nancy, age=25]],
+         * true=[Person[name=Sara, age=20], Person[name=Nancy, age=22], Person[name=Kitty, age=4], Person[name=Cinderella, age=10]]}
+         */
+
         System.out.println(groupPeopleBasedOnTheirNames());
+        /*
+         * {Batman=[Person[name=Batman, age=9]], Kitty=[Person[name=Kitty, age=4]],
+         * Nancy=[Person[name=Nancy, age=22], Person[name=Nancy, age=25]], Cathy=[Person[name=Cathy, age=23]],
+         * Sara=[Person[name=Sara, age=20]],
+         * Chinnu=[Person[name=Chinnu, age=1]], Cinderella=[Person[name=Cinderella, age=10]]}
+         */
+
+        System.out.println(groupPeopleByNameAndReturnAListOfAges());
+        //{Batman=[9], Kitty=[4], Nancy=[22, 25], Cathy=[23], Sara=[20], Chinnu=[1], Cinderella=[10]}
     }
 
     public static List<Person> getPeople() {
@@ -46,8 +66,13 @@ public class MainClass {
                 .collect(partitioningBy(person -> person.age() % 2 == 0));
     }
 
-    public static Map<String, List<Person>> groupPeopleBasedOnTheirNames(){
+    public static Map<String, List<Person>> groupPeopleBasedOnTheirNames() {
         return getPeople().stream()
                 .collect(groupingBy(Person::name));
+    }
+
+    public static Map<String, List<Integer>> groupPeopleByNameAndReturnAListOfAges() {
+        return getPeople().stream()
+                .collect(groupingBy(Person::name, mapping(Person::age, toList())));
     }
 }
