fb2f6d482cd422ca87ae74c2b673b310fa8d6aed
roshinivr <roshinirajesh1@gmail.com>
2023-01-05 21:08:19 +0000

    Use partitioningBy in collectors

--- src/main/java/com/example/demo/practice/streams/MainClass.java
+++ src/main/java/com/example/demo/practice/streams/MainClass.java
@@ -3,15 +3,20 @@ package com.example.demo.practice.streams;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.partitioningBy;
+
 public class MainClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         System.out.println(getOddAge());
         System.out.println(getEvenAge());
+        System.out.println(getByUsingPartitioningByForEvenAndOddAges());
     }
 
-    public static List<Person> getPeople(){
+    public static List<Person> getPeople() {
         return List.of(new Person("Sara", 20),
                 new Person("Nancy", 22),
                 new Person("Cathy", 23),
@@ -21,15 +26,20 @@ public class MainClass {
                 new Person("Cinderella", 10));
     }
 
-    public static List<Person> getOddAge(){
+    public static List<Person> getOddAge() {
         return getPeople().stream()
                 .filter(e -> e.age() % 2 != 0)
                 .collect(Collectors.toList());
     }
 
-    public static List<Person> getEvenAge(){
+    public static List<Person> getEvenAge() {
         return getPeople().stream()
                 .filter(e -> e.age() % 2 == 0)
                 .collect(Collectors.toList());
+    }
+
+    public static Map<Boolean, List<Person>> getByUsingPartitioningByForEvenAndOddAges() {
+        return getPeople().stream()
+                .collect(partitioningBy(person -> person.age() % 2 == 0));
     }
 }
