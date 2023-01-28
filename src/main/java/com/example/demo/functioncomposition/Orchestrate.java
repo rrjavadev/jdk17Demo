3959cc3e0473a2c9d3dc04dda6dd3fb900bd3c07
roshinivr <roshinirajesh1@gmail.com>
2023-01-28 23:43:20 +0000

    Add orchestration layer

--- src/main/java/com/example/demo/functioncomposition/Orchestrate.java
+++ src/main/java/com/example/demo/functioncomposition/Orchestrate.java
@@ -1,17 +1,33 @@
 package com.example.demo.functioncomposition;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class Orchestrate {
+    final Function<FoodItem, FoodItem> foodItemFunction;
+
+    public Orchestrate(Function<FoodItem, FoodItem>... foodItems) {
+        foodItemFunction = Arrays.stream(foodItems)
+                .reduce(Function.identity(), Function::andThen);
+    }
+
+    public void startTasting(FoodItem foodItem) {
+        foodItemFunction.apply(foodItem);
+    }
+
     public static void main(String[] args) {
 
-        Function<FoodItem, FoodItem> curryItem = e -> new Curry().tasteIt(e);
-        Function<FoodItem, FoodItem> riceItem = e -> new Rice().tasteIt(e);
-        Function<FoodItem, FoodItem> friesItem = e -> new Fries().tasteIt(e);
+        Function<FoodItem, FoodItem> tasteCurry = e -> new Curry().tasteIt(e);
+        Function<FoodItem, FoodItem> tasteRice = e -> new Rice().tasteIt(e);
+        Function<FoodItem, FoodItem> tasteFries = e -> new Fries().tasteIt(e);
 
-        curryItem
-                .andThen(riceItem)
-                .andThen(friesItem)
-                .apply(new FoodItem("Fish", 5, "Indian"));
+//        curryItem
+//                .andThen(riceItem)
+//                .andThen(friesItem)
+//                .apply(new FoodItem("Fish", 5, "Indian"));
+//         or
+
+        new Orchestrate(tasteCurry, tasteRice, tasteFries)
+                .startTasting(new FoodItem("Fish", 5, "Indian"));
     }
 }
